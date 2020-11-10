83abf8d0f71480bf3770a7e1af79e26bb9d7da0e
Eric <eric.m.lombardo@gmail.com>
2020-11-10 12:51:05 -0500

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+# creates #introduction that introduces person by name
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+
+# creates #introduction_with_language and outputs phrase
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
